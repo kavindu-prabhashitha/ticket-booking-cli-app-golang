d74305b65159d1be3c7f7f067091b65d6f708f00
Kavindu Prabhashitha <donprabhashitha@gmail.com>
2023-08-21 01:02:13 +0530

    define arrays

--- main.go
+++ main.go
@@ -1,21 +1,42 @@
 package main
 
-import "fmt" 
+import "fmt"
 
-func main(){
+func main() {
 	var conferenceName = "Go Conference"
 	const confereceTickets = 50
 	var remainingTickets uint = 50
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 
 	//formated string with place holders ( variable reference)
 	fmt.Printf("Welcome to our %v booking applications \n", conferenceName)
-	fmt.Println("We have total of", confereceTickets, "tickets and",remainingTickets, "are still available")
+	fmt.Println("We have total of", confereceTickets, "tickets and", remainingTickets, "are still available")
 
-	var userName string
-	var userTickets int
+	var bookings [50]string
+
 	// ask user for their name
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets \n", userName,userTickets)
+	//fmt.Println(remainingTickets)
+	//fmt.Println(&remainingTickets)
+
+	//ask for name:
+	fmt.Println("Enter your first name :")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name :")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email :")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter no.of tickets :")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - uint(userTickets)
+	bookings[0] = firstName + " " + lastName
+	fmt.Printf("Thank you %v %v for buying %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v ", remainingTickets, conferenceName)
 }
