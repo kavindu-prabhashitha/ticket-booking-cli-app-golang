3eb2891e5a7506f7ee0540d593119409154c212f
Kavindu Prabhashitha <donprabhashitha@gmail.com>
2023-08-21 22:54:16 +0530

    main.go updated

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conferenceName = "Go Conference"
@@ -17,26 +20,38 @@ func main() {
 
 	var bookings []string
 
+	for {
+		fmt.Println("Enter your first name :")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Enter your last name :")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email :")
+		fmt.Scan(&email)
+
+		fmt.Println("Enter no.of tickets :")
+		fmt.Scan(&userTickets)
+
+		remainingTickets = remainingTickets - uint(userTickets)
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("Thank you %v %v for buying %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v ", remainingTickets, conferenceName)
+
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("These are all our bookings : %v \n ", firstNames)
+	}
+
 	// ask user for their name
 
 	//fmt.Println(remainingTickets)
 	//fmt.Println(&remainingTickets)
 
 	//ask for name:
-	fmt.Println("Enter your first name :")
-	fmt.Scan(&firstName)
 
-	fmt.Println("Enter your last name :")
-	fmt.Scan(&lastName)
-
-	fmt.Println("Enter your email :")
-	fmt.Scan(&email)
-
-	fmt.Println("Enter no.of tickets :")
-	fmt.Scan(&userTickets)
-
-	remainingTickets = remainingTickets - uint(userTickets)
-	bookings = append(bookings, firstName+" "+lastName)
-	fmt.Printf("Thank you %v %v for buying %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v ", remainingTickets, conferenceName)
 }
