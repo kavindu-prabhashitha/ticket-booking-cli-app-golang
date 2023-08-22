6626ab4bc3addff8813b360afd5d1a5690345451
Kavindu Prabhashitha <donprabhashitha@gmail.com>
2023-08-22 23:27:14 +0530

    functions created and readme.md updated

--- main.go
+++ main.go
@@ -9,42 +9,25 @@ func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
 	var remainingTickets uint = 50
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+
+	greetUsers(conferenceName, conferenceTickets, remainingTickets)
 
 	//formated string with place holders ( variable reference)
-	fmt.Printf("Welcome to our %v booking applications \n", conferenceName)
-	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
-
 	var bookings []string
 
 	for {
-		fmt.Println("Enter your first name :")
-		fmt.Scanln(&firstName)
+		firstName, lastName, email, userTickets := getUserInput()
+		isValidName, isValidEmail, isValidTicketNumber := validateUserInputs(firstName, lastName, email, userTickets, remainingTickets)
 
-		fmt.Println("Enter your last name :")
-		fmt.Scanln(&lastName)
-
-		fmt.Println("Enter your email :")
-		fmt.Scanln(&email)
-
-		fmt.Println("Enter no.of tickets :")
-		fmt.Scanln(&userTickets)
-
-		if userTickets <= remainingTickets {
+		if isValidEmail && isValidName && isValidTicketNumber {
 			remainingTickets = remainingTickets - uint(userTickets)
 			bookings = append(bookings, firstName+" "+lastName)
 
 			fmt.Printf("Thank you %v %v for buying %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
 			fmt.Printf("%v tickets remaining for %v ", remainingTickets, conferenceName)
 
-			firstNames := []string{}
-			for _, booking := range bookings {
-				var names = strings.Fields(booking)
-				firstNames = append(firstNames, names[0])
-			}
+			// function print first names
+			firstNames := getFirstNames(bookings)
 			fmt.Printf("These are all our bookings : %v \n ", firstNames)
 
 			noTicketsRemaining := remainingTickets == 0
@@ -54,17 +37,65 @@ func main() {
 				break
 			}
 		} else {
-			fmt.Printf("We only have %v tickets remaining , so you can't book %v tickets\n", remainingTickets, userTickets)
+			if !isValidName {
+				fmt.Println("first name or last name you entered is too short")
+			}
+
+			if !isValidEmail {
+				fmt.Println("email you entered is not valid")
+			}
+
+			if !isValidTicketNumber {
+				fmt.Println("number of tickets you entered is invalid")
+			}
+			//fmt.Printf("We only have %v tickets remaining , so you can't book %v tickets\n", remainingTickets, userTickets)
+			fmt.Println("Your input data is invalid, try again")
 			continue
 		}
 
 	}
 
-	// ask user for their name
+}
 
-	//fmt.Println(remainingTickets)
-	//fmt.Println(&remainingTickets)
+func greetUsers(conferenceName string, conferenceTickets uint, remainingTickets uint) {
+	fmt.Printf("Welcome to our %v booking applications \n", conferenceName)
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
+}
 
-	//ask for name:
+func getFirstNames(bookings []string) []string {
+	firstNames := []string{}
+	for _, booking := range bookings {
+		var names = strings.Fields(booking)
+		firstNames = append(firstNames, names[0])
+	}
+	return firstNames
+}
 
+func validateUserInputs(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+	return isValidName, isValidEmail, isValidTicketNumber
+}
+
+func getUserInput() (string, string, string, uint) {
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	fmt.Println("Enter your first name :")
+	fmt.Scanln(&firstName)
+
+	fmt.Println("Enter your last name :")
+	fmt.Scanln(&lastName)
+
+	fmt.Println("Enter your email :")
+	fmt.Scanln(&email)
+
+	fmt.Println("Enter no.of tickets :")
+	fmt.Scanln(&userTickets)
+
+	return firstName, lastName, email, userTickets
 }
